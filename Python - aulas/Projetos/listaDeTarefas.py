625adc230d25cc34e4ebbf25397e0c601b0bd5dd
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-02-28 18:52:10 -0300

    estrutura do projeto

---
+++ Python - aulas/Projetos/listaDeTarefas.py
@@ -0,0 +1,11 @@
+lista_de_tarefas = []
+
+while True:
+    menu = input("""
+        1 - Adicionar nova tarefa
+        2 - Ver todas as tarefas
+        3 - Excluir tarefa
+        4 - Filtrar por categoria
+        5 - Filtrar por prioridade
+
+""")
