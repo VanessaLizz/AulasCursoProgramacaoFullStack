1802f849c403de00e96a332ffac7a7b2250f17e6
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-05-12 15:58:42 -0300

    Variáveis ​​e Operadores

---
+++ Exercícios/js01/script.js
@@ -0,0 +1,30 @@
+// Some dois números! No seu script, digite console.log(23 + 97). Executar isso deve registrar 120.
+console.log(`O resultado da soma 23 + 97 é: ${23+97}`)
+
+// Agora, registre o valor da seguinte expressão: (4 + 6 + 9) / 77. O console deve registrar aproximadamente 0.24675.
+console.log(`O resultado da soma 4 + 6 + 9, dividido por 77 é: ${(4+6+9)/77}`)
+
+// Vamos usar algumas variáveis!
+// Adicione esta declaração à tag de script: let a = 10.
+// Abaixo, adicione console.log(a). Ao executar este comando, o console do navegador deverá registrar 10.
+let a = 10
+console.log(`O valor de a é: ${a}`)
+
+// Em seguida, reatribua a A, um valor numérico diferente. Registre anovamente e ele deverá mostrar o valor atualizado (o registro anterior ainda deverá mostrar o valor antigo, 10pois era anterior aà reatribuição).
+a = 20
+console.log(`O valor de a é: ${a}`)
+// Agora adicione ao final do script let b = 7 * a.
+// Registre o resultado b. Deve registrar o resultado de 7 multiplicado pelo valor que você atribuiu novamente a.
+let b = 7 * a
+console.log(`O valor de 7 * a é: ${b}`)
+// Experimente esta sequência:
+// Declare uma variável constante max com o valor 57.
+const max = 57
+// Declare outra variável constante actuale atribua um valor a ela max - 13.
+const actuale = max - 13
+// Declare outra variável constante percentage e atribua um valor a ela actual / max.
+const percentage = actuale / max
+// Agora, se você fizer login percentage, deverá ver um valor no console como este 0.7719:
+console.log(`O valor de max é: ${max}`)
+console.log(`O valor de max - 13 é: ${actuale}, sendo o valor de actuale`)
+console.log(`O valor de max / actuale é: ${percentage}`)
