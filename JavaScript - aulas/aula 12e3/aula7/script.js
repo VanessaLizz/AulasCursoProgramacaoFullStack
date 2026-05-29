b36ba26f01f2838cb8646625d4a1a111b128a2a5
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-05-29 08:50:44 -0300

    exemplo de foreach

--- JavaScript - aulas/aula 12e3/aula7/script.js
+++ JavaScript - aulas/aula 12e3/aula7/script.js
@@ -13,3 +13,32 @@ analisar.addEventListener("click", () => {
     meus_salarios.appendChild(novo_item)
 })
 
+// EXEMPLO DE FOREACH
+const salario = document.querySelector("#salario") // INPUT
+const analisar = document.querySelector("#analisar") // BUTTON
+const meus_salarios = document.querySelector("#meus_salarios") // UL
+
+analisar.addEventListener("click", () => {
+    const novo_item = document.createElement('li')
+    novo_item.textContent = `R$ ${salario.value}`
+    if(Number(salario.value) > 1600){
+        novo_item.style.color = 'green'
+    }else{
+        novo_item.style.color = 'red'
+    }
+    meus_salarios.appendChild(novo_item)
+})
+
+
+const salarios_do_banco = [1598.75, 2500, 6799.99, 800, 600, 1000]
+
+salarios_do_banco.forEach((element)=>{ // for que vai dar 6 voltas.
+    const novo_item = document.createElement('li')
+    novo_item.textContent = `R$ ${element}`
+    if(element > 1600){
+        novo_item.style.color = 'green'
+    }else{
+        novo_item.style.color = 'red'
+    }
+    meus_salarios.appendChild(novo_item)
+})
