709aa469a825e3c261a97497358d92f51dbe5317
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-05-29 08:51:39 -0300

    exemplo de keydown

--- JavaScript - aulas/aula 12e3/aula7/script.js
+++ JavaScript - aulas/aula 12e3/aula7/script.js
@@ -42,3 +42,37 @@ salarios_do_banco.forEach((element)=>{ // for que vai dar 6 voltas.
     }
     meus_salarios.appendChild(novo_item)
 })
+
+// KEYDOWN
+const salario = document.querySelector("#salario") // INPUT
+const analisar = document.querySelector("#analisar") // BUTTON
+const meus_salarios = document.querySelector("#meus_salarios") // UL
+
+analisar.addEventListener("click", () => {
+    const novo_item = document.createElement('li')
+    novo_item.textContent = `R$ ${salario.value}`
+    if(Number(salario.value) > 1600){
+        novo_item.style.color = 'green'
+    }else{
+        novo_item.style.color = 'red'
+    }
+    meus_salarios.appendChild(novo_item)
+    salario.value = ""
+    salario.focus()
+})
+
+
+salario.addEventListener("keydown", (event)=>{
+    if(event.key === 'Enter'){
+        const novo_item = document.createElement('li')
+        novo_item.textContent = `R$ ${salario.value}`
+        if(Number(salario.value) > 1600){
+            novo_item.style.color = 'green'
+        }else{
+            novo_item.style.color = 'red'
+        }
+        meus_salarios.appendChild(novo_item)
+        salario.value = ""
+        salario.focus()
+    }
+})
