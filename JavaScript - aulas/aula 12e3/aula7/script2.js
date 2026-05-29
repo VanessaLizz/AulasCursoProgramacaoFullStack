d2edba9dc47b548c7ad390ecafa5e4ad0962a125
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-05-29 08:53:58 -0300

    eventos de teclado

--- JavaScript - aulas/aula 12e3/aula7/script2.js
+++ JavaScript - aulas/aula 12e3/aula7/script2.js
@@ -16,3 +16,22 @@ botao.addEventListener("mouseout", ()=>{
     console.log("O mouse saiu")
 })
 
+// EVENTOS DE TECLADO
+const caixinha = document.querySelector("#caixinha")
+
+// caixinha.addEventListener("keydown", (event)=>{
+//     console.log(event.key)
+// })
+
+// caixinha.addEventListener("keyup", (event)=>{
+//     console.log(event.key)
+// })
+
+caixinha.addEventListener("change", (event)=>{
+    console.log(event.target.value)
+})
+
+
+caixinha.addEventListener("input", (event)=>{
+    console.log(event.target.value)
+})
